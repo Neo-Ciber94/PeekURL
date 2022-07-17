86c0be78421fa3caefc5a2db5745836659b83a1b
Freddy L <neo-ciber_94@hotmail.com>
2022-07-17 16:26:16 -0400

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
 import { trpc } from "../utils/trpc";
 
 export default function IndexPage() {
-  const hello = trpc.useQuery(["hello.sayhello", { text: "client" }]);
-  if (!hello.data) {
-    return <div>Loading...</div>;
-  }
   return (
     <div>
-      <p>{hello.data.greeting}</p>
+      <p>{"Hello"}</p>
     </div>
   );
 }
