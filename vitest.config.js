32867f0a38eb1063d0330633afd9e731904a64ca
Freddy L <neo-ciber_94@hotmail.com>
2022-07-23 16:13:03 -0400

    Added tests

--- vitest.config.js
+++ vitest.config.js
@@ -2,11 +2,17 @@
 
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
   },
 });
