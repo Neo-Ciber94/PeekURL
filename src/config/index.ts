46b764ae6825663f3d97bd2ae3396c9cc39a3e1c
Freddy L <neo-ciber_94@hotmail.com>
2022-07-31 19:38:47 -0400

    Updated config variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,11 +2,13 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3000";
 
+const environment = process.env.NEXT_PUBLIC_VERCEL_ENV || process.env.NODE_ENV;
+
 /**
  * Configuration for application.
  */
 export const Config = Object.freeze({
-  isDevelopment: process.env.NODE_ENV === "development",
+  isDevelopment: environment === "development",
   BASE_URL: baseUrl,
   API_URL: `${baseUrl}/api`,
   IP_STACK_API_KEY: process.env.IP_STACK_API_KEY,
