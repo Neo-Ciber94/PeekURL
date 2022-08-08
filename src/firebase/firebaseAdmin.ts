d52fd7eb4211d75abe26b7f226157cb77c6eebeb
Freddy L <neo-ciber_94@hotmail.com>
2022-08-07 21:53:19 -0400

    Updated firebase admin

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -1,24 +1,34 @@
-import firebaseAdmin, { initializeApp, cert, getApps } from "firebase-admin/app";
+import firebaseAdmin, {
+  initializeApp,
+  cert,
+  getApps,
+} from "firebase-admin/app";
 import { getSecretProvider } from "src/services/firebase-secret-provider";
 import logger from "src/logging";
+import retry from "@utils/retry";
 
 let app: firebaseAdmin.App | undefined;
 
 export async function getFirebaseAdmin(): Promise<firebaseAdmin.App> {
-    if (app == null && getApps().length === 0) {
-        const provider = getSecretProvider();
-        const secret = await provider.getSecret();
+  if (app == null && getApps().length === 0) {
+    const provider = getSecretProvider();
+    const secret = await retry(
+      () => provider.getSecret(),
+      retry.exponential(5, (_retryCount, _err, ms) => {
+        logger.warn(`Failed to get firebase secret, retrying in ${ms}ms`);
+      })
+    );
 
-        app = initializeApp({
-            credential: cert({
-                projectId: secret.project_id,
-                clientEmail: secret.client_email,
-                privateKey: secret.private_key,
-            }),
-        });
+    app = initializeApp({
+      credential: cert({
+        projectId: secret.project_id,
+        clientEmail: secret.client_email,
+        privateKey: secret.private_key,
+      }),
+    });
 
-        logger.info("Initialized server side firebase");
-    }
+    logger.info("Initialized server side firebase");
+  }
 
-    return app!;
+  return app!;
 }
